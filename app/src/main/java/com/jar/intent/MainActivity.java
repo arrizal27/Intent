407c22281c27bed28b958bb188ea89068a78b00c
Fajar Sidiq Arrizal <fajarsidiq3000@gmail.com>
2020-08-22 15:33:05 +0700

    first commit

---
+++ app/src/main/java/com/jar/intent/MainActivity.java
@@ -0,0 +1,27 @@
+package com.jar.intent;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        final EditText name = findViewById(R.id.edittext);
+        Button button = findViewById(R.id.submit_button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(MainActivity.this,MainActivity2.class);
+                i.putExtra("name",name.getText().toString());
+                startActivity(i);
+            }
+        });
+    }
+}
